c89725bd5ff2521da3eb88057ae58e21533043ec
ssc1993 <18303093295@163.com>
2019-09-02 17:40:16 +0800

    添加书籍短评的功能查询

--- show-info-web/src/main/java/com/ssc/showinfo/web/controller/BookController.java
+++ show-info-web/src/main/java/com/ssc/showinfo/web/controller/BookController.java
@@ -2,13 +2,18 @@ package com.ssc.showinfo.web.controller;
 
 import com.github.pagehelper.PageInfo;
 import com.ssc.showinfo.biz.service.BookService;
+import com.ssc.showinfo.biz.service.BookShortCommentService;
+import com.ssc.showinfo.dao.entity.Book;
 import com.ssc.showinfo.dao.entity.BookInfo;
+import com.ssc.showinfo.dao.entity.BookShortComment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @program: show-info
@@ -24,6 +29,9 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private BookShortCommentService bookShortCommentService;
 
     @GetMapping("test")
     public String test() {
@@ -59,13 +67,20 @@ public class BookController {
      * @return
      */
     @GetMapping("queryBookInfoById/{id}")
-    public ResponseEntity<BookInfo> queryBookInfoById(@PathVariable(value = "id") Integer id) {
+    public ResponseEntity<Book> queryBookInfoById(@PathVariable(value = "id") Integer id) {
         if (logger.isInfoEnabled()) {
             logger.info("开始调用queryBookInfoById接口，id={}", id);
         }
         try {
             BookInfo bookInfo = bookService.queryById(id);
-            return ResponseEntity.ok(bookInfo);
+            BookShortComment bookShortComment = new BookShortComment();
+            bookShortComment.setId(0);
+            bookShortComment.setBookId(id);
+            List<BookShortComment> list = bookShortCommentService.queryListByRecord(bookShortComment);
+            Book book = new Book();
+            book.setBookInfo(bookInfo);
+            book.setBookShortComments(list);
+            return ResponseEntity.ok(book);
         } catch (Exception e) {
             logger.error("调用queryBookInfoById接口失败！", e);
         }
