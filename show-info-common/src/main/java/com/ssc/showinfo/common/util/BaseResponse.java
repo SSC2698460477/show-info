024f789633d3196e76102c6c132c9efa63105c38
ssc1993 <18303093295@163.com>
2019-10-01 21:03:10 +0800

    增加响应信息实体类

---
+++ show-info-common/src/main/java/com/ssc/showinfo/common/util/BaseResponse.java
@@ -0,0 +1,59 @@
+package com.ssc.showinfo.common.util;
+
+/**
+ * @program: show-info
+ * @description: API接口的基础返回类
+ * @author: ssc
+ * @create: 2019/9/30 21:51
+ **/
+public class BaseResponse<T> {
+
+    /**
+     * 是否成功
+     */
+    private boolean success;
+
+    /**
+     *说明
+     */
+    private String msg;
+
+    /**
+     *返回数据
+     */
+    private T data;
+
+    public BaseResponse(){
+
+    }
+
+    public BaseResponse(boolean success, String msg, T data) {
+        this.success = success;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
